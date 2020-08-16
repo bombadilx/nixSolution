fa0f271fd8d51d7b66a28b9df78e356997e52fad
Artur Degtyar <tom.bom2309@gmail.com>
2020-08-16 15:28:32 +0300

    first commit

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello NixSolution" ;
+
+$result = '';
+$data = ['H', 'E', 'L', 'L', 'O'];
+foreach ($data as $i => $letter) {
+    if (strlen($result) === 4) {
+        $result .= ' template';
+        break;
+    }
+    if ($i >= 0) {
+        $result .= strtolower($letter);
+    }
+}
+echo ucfirst(str_replace('template', 'YEAH', $result));
