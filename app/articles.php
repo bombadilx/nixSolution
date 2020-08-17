365259b1b8a204f57881df30ed7d497575a108fd
Artur Degtyar <tom.bom2309@gmail.com>
2020-08-17 15:17:49 +0300

    На странице постов отображаются посты, которые хранятся в отдельном файле

---
+++ app/articles.php
@@ -0,0 +1,30 @@
+<?php 
+
+$articles = [
+    $article = [
+        "id" => "1",
+        "title" => "Заголовок 1",
+        "image" => "image/image1.png",
+        "text" => "Текст статьи под номером 1",
+    ],
+    
+    $article = [
+        "id" => "2",
+        "title" => "Заголовок 2",
+        "image" => "image/image1.png",
+        "text" => "Текст статьи под номером 2",
+    ],
+    
+    $article = [
+        "id" => "3",
+        "title" => "Заголовок 3",
+        "image" => "image/image1.png",
+        "text" => "Текст статьи под номером 3",
+    ],
+    $article = [
+        "id" => "4",
+        "title" => "Заголовок 4",
+        "image" => "image/image1.png",
+        "text" => "Текст статьи под номером 4",
+    ],
+];
--- app/post-list.php
+++ app/post-list.php
@@ -1,20 +1,25 @@
 <?php
 
-require_once('header.php');
-require_once('navigation.php');
+require_once('../components/header.php');
+require_once('../components/navigation.php');
+require_once('articles.php');
 
 ?>
 
 <div class="post-list">
-    <div class="post-list-title">
-        Заголовок
+    <? foreach ($articles as $value): ?>
+    <div class="article">
+        <div class="post-list-title">
+            <?= $value['title']; ?>
+        </div>
+        <div class="post-list-image">
+            <img width="200" src="<?= $value['image'];?>" alt="картинка-поста">
+        </div>
+        <div class="post-list-text">
+            <?= $value['text'];?>
+        </div>
     </div>
-    <div class="post-list-image">
-        <img src="" alt="картинка-поста">
-    </div>
-    <div class="post-list-text">
-        Текст поста
-    </div>
+    <?php endforeach ?>
 </div>
 
-<?php require_once('footer.php'); ?>
+<?php require_once('../components/footer.php'); ?>
